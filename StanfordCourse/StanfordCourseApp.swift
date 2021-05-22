6d30f27ff515bec0a385517c7dfe5db5b78ceaa8
Juli0 <julio.fils1@gmail.com>
2021-05-22 10:37:27 -0400

    Initial Commit

---
+++ StanfordCourse/StanfordCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordCourseApp.swift
+//  StanfordCourse
+//
+//  Created by Julio Jean Fils on 22/5/21.
+//
+
+import SwiftUI
+
+@main
+struct StanfordCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
